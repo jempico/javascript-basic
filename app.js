3fdd6d196dd8b976e5b3d13cbdfdb33e7122976a
jempico <jpfilarca@gmail.com>
2021-01-18 12:48:32 +0100

    Add files via upload
    added more exercices

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+console.log('Hola Mundo');
+alert('Me llamo Jemimah');
+let myName = "Jemimah";
+let surName = "Pico";
+
+let num1 = 10;
+let num2 = 5;
+console.log(num1 + num2);
+
+let nota_examen = 4;
+if (nota_examen < 5) {
+    alert(`OOOhhh has suspendido el examen con un ${nota_examen}`); 
+} else if (nota_examen >= 5) {
+    alert(`Enhorabuena, has aprobado el examen con un ${nota_examen}`);
+};
+
+
 function makeOperation(n1, operator, n2) {
     let result = ''
     
@@ -15,10 +32,15 @@ function makeOperation(n1, operator, n2) {
                 result = n1/n2;
             }
         }
-        console.log(result);
+        console.log(`El resultado de ${n1} ${operator} ${n2} es ${result}`);
     }
     else {
         console.log('Please enter a number');
     } 
   }
 
+makeOperation(10, "+", 5);
+makeOperation(10, "-", 5);
+makeOperation(10, "x", 5);
+makeOperation(10, "/", 5);
+makeOperation(10, "÷", 0);
